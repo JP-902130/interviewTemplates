bcd169b65a76386fc5977caa489f936cc84fa2fa
jingchengpeng@yahoo.com <jingchengpeng@yahoo.com>
2023-02-08 22:08:03 -0500

    more on midpoint

--- linkedList/findMidPoint.py
+++ linkedList/findMidPoint.py
@@ -4,8 +4,8 @@ class Node:
         self.next = next
 
 
+# This version returns 2 for the list 1 2 3 4
 head = Node()
-
 slow = head
 fast = head.next
 while fast and fast.next:
@@ -13,3 +13,17 @@ while fast and fast.next:
     slow = slow.next
 
 endOfFirstHalf = slow
+
+# This version returns 3 for the list 1 2 3 4
+head = Node()
+slow = head
+fast = head
+while fast and fast.next:
+    fast = fast.next.next
+    slow = slow.next
+
+endOfFirstHalf = slow
+
+'''
+1) Both versions work, but sometimes one works better then the other. 
+'''
