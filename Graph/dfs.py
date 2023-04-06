0fafdbd5bcff2cfb7de16f7d006a322f03630b6b
jingchengpeng@yahoo.com <jingchengpeng@yahoo.com>
2023-04-06 09:52:10 -0400

    DFS improvement

--- Graph/dfs.py
+++ Graph/dfs.py
@@ -1,22 +1,35 @@
-visited = set()
-directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
 
+        visited = set()
+        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
 
-def isInBound(r, c):
-    return 0 <= r <= len(grid) - 1 and 0 <= c <= len(grid[0]) - 1
+        def isInBound(r, c):
+            return 0 <= r <= len(grid) - 1 and 0 <= c <= len(grid[0]) - 1
 
+        def dfs(r, c):
 
-def dfs(r, c):
+            # Boundary check
+            if isInBound(r, c) == False:
+                return
+            # Visited check
+            if (r, c) in visited:
+                return
+            # Some other condition
+            if grid[r][c] == '0':
+                return
+            visited.add((r, c))
 
-    for dir in directions:
-        new_x = r + dir[0]
-        new_y = c + dir[1]
-        if isInBound(new_x, new_y) and ('''the condition to test if the next neighbor is visitable''') and (r, c) not in visited:
-            visited.add(new_x, new_y)
-            dfs(new_x, new_y)
+            for dir in directions:
+                newR = dir[0] + r
+                newC = dir[1] + c
+                dfs(newR, newC)
 
-
-for r in range(len(grid)):
-    for c in range(len(grid[0])):
-        if ('''condition''') and (r, c) not in visited:
-            dfs(r, c)
+        count = 0
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                # Things that might be modified here
+                if (r, c) not in visited and grid[r][c] == '1':
+                    count += 1
+                    dfs(r, c)
+        return count
